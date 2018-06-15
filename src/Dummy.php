3ff49d93c87bb5fceb38902e26f4d453ea13ea4c
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-06-15 17:48:34 +0200

    Dummy package

---
+++ src/Dummy.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace PHPDIDefinitions;
+
+final class Dummy
+{
+}
---
+++ src/dummy.php
@@ -0,0 +1,9 @@
+<?php
+
+use PHPDIDefinitions\Dummy;
+
+return [
+    Dummy::class => function () {
+        return new Dummy();
+    },
+];
